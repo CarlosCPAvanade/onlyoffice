a123512dd162ead664197f91eabcb2dfbdf46e8f
CarlosCPAvanade <114080589+CarlosCPAvanade@users.noreply.github.com>
2026-05-06 13:13:48 +0200

    subida con controller

---
+++ OnlyOfficeCallback.cs
@@ -0,0 +1,8 @@
+namespace OnlyOfficeDemo.Api.Models;
+
+public class OnlyOfficeCallback
+{
+    public string? key { get; set; }
+    public int status { get; set; }
+    public string? url { get; set; }
+}
